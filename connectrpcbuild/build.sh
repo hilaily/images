95e94b8b34551e4e5ad5fb3b06bd75af5b446176
laily <i@laily.net>
2024-12-12 21:24:14 +0800

    chore: update mysql

--- connectrpcbuild/build.sh
+++ connectrpcbuild/build.sh
@@ -1,4 +1,4 @@
 mkdir _data
 cd _data
 wget -O go.tar.gz https://go.dev/dl/go1.23.3.linux-amd64.tar.gz
-wget -O install_node.sh https://deb.nodesource.com/setup_23.x
+wget -O install_node.sh https://deb.nodesource.com/setup_20.x
