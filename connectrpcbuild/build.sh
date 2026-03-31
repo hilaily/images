5b33fc47f6fdefe53989d5a2d8e4ebb538c738fc
laily <i@laily.net>
2026-03-31 12:18:05 +0800

    feat: update connectrpc build

--- connectrpcbuild/build.sh
+++ connectrpcbuild/build.sh
@@ -1,4 +1,14 @@
-mkdir _data
+mkdir -p _data
 cd _data
-wget -O go.tar.gz https://go.dev/dl/go1.23.3.linux-amd64.tar.gz
 wget -O install_node.sh https://deb.nodesource.com/setup_20.x
+cd ..
+
+# Go tarball is now downloaded at build time based on TARGETARCH,
+# no need to pre-download it.
+
+# Multi-platform build (amd64 + arm64):
+# docker buildx build --platform linux/amd64,linux/arm64 -t <image_name> .
+#
+# Single-platform build:
+# docker buildx build --platform linux/amd64 -t <image_name> .
+# docker buildx build --platform linux/arm64 -t <image_name> .
